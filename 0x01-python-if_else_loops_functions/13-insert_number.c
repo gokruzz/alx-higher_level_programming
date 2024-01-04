dd4ae644c3a038cf9de6d88e560fd69b3e3eb046
gokruzz <johnsonodukoyatemitope@gmail.com>
2024-01-03 22:36:00 -0800

    .

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -14,7 +14,7 @@ listint_t *insert_node(listint_t **head, int number)
     listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
     if (new_node == NULL)
     {
-        return NULL;
+        return (NULL);
     }
     new_node->data = number;
     new_node->next = NULL;
@@ -23,6 +23,15 @@ listint_t *insert_node(listint_t **head, int number)
     {
         new_node->next = *head;
         *head = new_node;
-        return new_node;
+        return (new_node);
     }
+    listint_t *current = *head;
+    while (current->next != NULL && current->next->data < number)
+    {
+        current = current->next;
+    }
+    new_node->next = current->next;
+    current->next = new_node;
+
+    return (new_node);
 }
