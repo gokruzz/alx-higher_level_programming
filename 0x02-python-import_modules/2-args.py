af0a8833f6f9d203b07c7a369c224a99afc80691
gokruzz <johnsonodukoyatemitope@gmail.com>
2024-01-04 17:31:18 -0800

    import modules.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-
-if __name__ == '__main__':
-    from sys import argv
-    n = len(argv) - 1
-    if n == 0:
-        print("{} arguments.". format(n))
-    elif n == 1:
-        print("{} argument:".format(n))
-        print("1: {}".format(argv[1]))
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv)
+    if(args <= 1):
+        print("{} arguments.".format(args - 1))
+    elif(args == 2):
+        print("{} argument:".format(args - 1))
     else:
-        print("{} arguments:".format(n))
-        i = 1
-        for arg in argv[1:]:
-            print("{}: {}".format(i, arg))
-            i += 1
+        print("{} arguments:".format(args - 1))
+    for x in range(1, args):
+        print("{}: {}".format(x, sys.argv[x]))
