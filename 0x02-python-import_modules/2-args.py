7a9e8f741d6c4ba70f61c6bda75e97938a8c831e
gokruzz <johnsonodukoyatemitope@gmail.com>
2024-01-04 18:37:33 -0800

    impory

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    args = len(sys.argv) - 1
-    if args == 0:
-        print("{} 0 arguments.".format(args))
-    elif args == 1:
-         print("{} 1 argument:".format(args))
-    else:
-        print("{} arguments:".format(args))
-    for x in range(args):
-        print("{}: {:s}".format(x + 1, argv[x + 1]))
+from sys import argv
+args = len(sys.argv) - 1
+if args == 0:
+print("{} 0 arguments.".format(args))
+elif args == 1:
+print("{} 1 argument:".format(args))
+else:
+print("{} arguments:".format(args))
+for x in range(args):
+print("{}: {:s}".format(x + 1, argv[x + 1]))
