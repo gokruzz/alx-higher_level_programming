698738082ed90783d5d0209e7f87889376bbc84f
gokruzz <johnsonodukoyatemitope@gmail.com>
2024-04-15 09:47:10 -0700

    error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
 """
-Module that connects a python script to a database
+Lists all states from the database hbtn_0e_0_usa
 """
+import sys
+import MySQLdb
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    import MySQLdb
-    from sys import argv
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states;")
+    states = cur.fetchall()
 
-    # Connect database using command-line arguments
-    my_db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2],
-                            db=argv[3], port=3306)
-    # Create cursor obj to interact with database
-    my_cursor = my_db.cursor()
-
-    # Execute a SELECT query to fetch data
-    my_cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
-
-    # fetch all the data returned by the query
-    my_data = my_cursor.fetchall()
-
-    # Iterate through the fetched data and print each row
-    for row in my_data:
-        print(row)
-
-    # Close all cursors
-    my_cursor.close()
-
-    # Close all databases
-    my_db.close()
+    for state in states:
+        print(state)
